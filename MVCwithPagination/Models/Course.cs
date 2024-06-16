dfaf4d390ab55ca15941019c029b586f395da0e3
Suraysen Thiyagarasu <suraysen@yahoo.com>
2024-06-16 22:15:22 +0800

    fixes

--- MVCwithPagination/Models/Course.cs
+++ MVCwithPagination/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace MVCwithPagination.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("Course Id")]
         public int CourseID { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
